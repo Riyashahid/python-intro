074bd4ba334a11a742e45e46957d92773d02db27
Riya Shahid Qureshi <139568493+Riyashahid@users.noreply.github.com>
2023-07-21 20:27:15 +0500

    Add files via upload

---
+++ func.py
@@ -0,0 +1,38 @@
+# def print_fixtech():
+#     print("we are learning python")
+
+
+# print_fixtech()
+# print_fixtech()
+# print_fixtech()
+
+# def artist(text):
+#     print(text)
+#     print(text)
+#     print(text)
+#     print(text)
+#     print(text)
+    
+# artist(input("enter ur text")) 
+
+# def variables():
+#     x=10
+#     y=5
+#     z=11
+#     print(x)
+#     print(x)
+#     print(x)
+#     print(y)
+#     print(y)
+#     print(y)
+#     print(z)
+#     print(z)
+#     print(z)
+
+# variables()
+
+def rain_chances(percentage):
+    percentage=percentage+10 
+    return percentage
+print(rain_chances(int(input("enter the percentag"))))
+print(type(percentage))
